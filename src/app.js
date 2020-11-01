33b0ef268782533229d1f31d67c499f99f2db8f8
macbookapp <macbook_app@icloud.com>
2020-11-01 12:10:41 +0100

    Cambios en nombres de directorios + añadido configuracion para variables de entorno

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
-import indexRutas from '../Rutas/indexRutas'
+import indexRutas from '../routes/indexRutas'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
+
+require("dotenv").config(); // Variables de entorno
 
 const app = express();
 const port = process.env.PORT || 4000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Conexión a Base de datos
+const uri = `mongodb://127.0.0.1:27017/${process.env.DBNAME}`;
+const opciones = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose
+  .connect(uri, opciones)
+  .then(() => console.log(`Conectado a mongodb: database => ${process.env.DBNAME}`))
+  .catch((error) => console.log("Error de conexión", error));
+
 
 
 //Rutas
